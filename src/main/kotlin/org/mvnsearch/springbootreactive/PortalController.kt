de953052072f7722aac58869968c676733abdd5e
linux_china <linux_china@hotmail.com>
2017-12-28 10:36:59 +0800

    polishing

--- src/main/kotlin/org/mvnsearch/springbootreactive/PortalController.kt
+++ src/main/kotlin/org/mvnsearch/springbootreactive/PortalController.kt
@@ -4,8 +4,6 @@ import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import reactor.core.publisher.Mono
-import reactor.core.publisher.toMono
 
 /**
  * portal controller
@@ -16,8 +14,8 @@ import reactor.core.publisher.toMono
 class PortalController {
 
     @GetMapping("/")
-    fun index(request: ServerHttpRequest, model: Model): Mono<String> {
+    fun index(request: ServerHttpRequest, model: Model): String {
         model.addAttribute("nick", "雷卷")
-        return "index".toMono()
+        return "index"
     }
 }
